c1c7f2a4c318e856c9a0a432fa72993ad0bd7878
necheporenko_s_iu <neceporenkostepan@gmail.com>
2022-01-12 09:56:46 +0300

    finished logging

--- conveyor/ItemsProcessor.py
+++ conveyor/ItemsProcessor.py
@@ -1,3 +1,4 @@
+import os
 from loguru import logger
 from abc import ABCMeta, abstractmethod
 
@@ -7,15 +8,30 @@ from . import Item, ItemsReceiver, Transaction
 
 class ItemsProcessor(ItemsReceiver, metaclass=ABCMeta):
 
-	def __init__(self, *args, logger=logger, **kwargs) -> None:
+	def __init__(self, *args, logs_dir='conveyor_log', **kwargs) -> None:
 
 		super().__init__(*args, **kwargs)
 		
-		self.logger = logger
+		self.logs_dir = logs_dir
 
 	@abstractmethod
 	def processItem(self, item: Item) -> Transaction:
 		pass
+	
+	def log(self, item, result):
+
+		chain_id = item.chain_id.replace(':', '_').replace(' ', '_')
+
+		file_name = f'{chain_id}.log'
+		file_path = os.path.join(self.logs_dir, file_name)
+		if not os.path.exists(file_path):
+			logger.add(
+				file_path, 
+				filter=lambda r: r['extra']['id'] == chain_id,
+				format='{time:YYYY-MM-DD HH:mm:ss.SSS} | <level>{message}</level>'
+			)
+
+		logger.bind(id=f'{chain_id}').info(f'{self.__class__.__name__} => {result}')
 
 	def __call__(self) -> list:
 
@@ -24,11 +40,7 @@ class ItemsProcessor(ItemsReceiver, metaclass=ABCMeta):
 		for i in self.receiveItems():
 			
 			i_result = self.processItem(i).execute()
-			i_info = {
-				'id': i.id,
-				'chain_id': i.chain_id
-			}
-			self.logger.info(f'{self.__class__.__name__} {i_info} => {i_result}')
+			self.log(i, i_result)
 
 			result.append(i_result)
 
