87172e604c9ad7c3c96d0a1825d1538b0a5c7cce
necheporenko_s_iu <neceporenkostepan@gmail.com>
2022-01-31 15:34:09 +0300

    added delete logging

--- conveyor/item_repositories_loggers/DefaultItemRepositoryLogger.py
+++ conveyor/item_repositories_loggers/DefaultItemRepositoryLogger.py
@@ -11,6 +11,8 @@ class DefaultItemRepositoryLogger(ItemRepositoryLogger):
 
 	def __init__(self, db: Database, log_table_name: str='conveyor_log'):
 
+		self.db = db
+
 		self.model = Model(db, log_table_name, {
 			'date': DateTimeField(),
 			'type': FixedCharField(max_length=63),
@@ -37,4 +39,17 @@ class DefaultItemRepositoryLogger(ItemRepositoryLogger):
 		self._log_item(item)
 
 	def delete(self, type, id):
-		return
+
+		model = Model(self.db, type)
+		if not model:
+			return None
+
+		item_row = model.select().where(model.id==id).get()
+
+		self._log_item(Item(
+			type=type,
+			status='',
+			id=item_row.id,
+			chain_id=item_row.chain_id,
+			data_digest=item_row.data_digest
+		))
