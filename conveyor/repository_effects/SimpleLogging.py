44ba2b85ad701f371b24c6f81c4b98b19c999cfe
necheporenko_s_iu <neceporenkostepan@gmail.com>
2022-02-09 12:51:36 +0300

    replace loguru with prints with colorama in SimpleLogging

--- conveyor/repository_effects/SimpleLogging.py
+++ conveyor/repository_effects/SimpleLogging.py
@@ -1,69 +1,25 @@
-import sys
-from loguru import logger
-from threading import Lock
+import colorama
+from datetime import datetime
 
 from .. import RepositoryEffect
 
 
 
-@lambda C: C()
-class lock:
-
-	def __init__(self):
-		self.lock = Lock()
-	
-	def __call__(self):
-		return self.lock
-
-
 class SimpleLogging(RepositoryEffect):
 
-	def __init__(
-		self,
-		sink=sys.stderr,
-		colors={
-			'create': 'green',
-			'update': 'blue',
-			'delete': 'red'
-		},
-		remove_default_handler=True
-	):
-
-		if remove_default_handler:
-			try:
-				logger.remove(0)
-			except ValueError:
-				pass
-		
-		self.loggers = {}
-		with lock():
-
-			for h in logger._core.handlers.values():
-				if not h._filter:
-					h._filter = lambda r: not 'conveyor' in r['extra']
-
-			for action in ['create', 'update', 'delete']:
-				
-				filter = eval(f"lambda r: ('conveyor' in r['extra']) and (r['extra']['conveyor'] == '{action}')")
-				
-				if all([
-					(not h._filter) or
-					(h._filter.__code__ != filter.__code__)
-					for h in logger._core.handlers.values()
-				]):
-					logger.add(
-						sink,
-						format=f'{{time:YYYY-MM-DD HH:mm:ss.SSS}} | <{colors[action]}>{{message}}</{colors[action]}>',
-						filter=filter
-					)
-				
-				self.loggers[action] = logger.bind(conveyor=action)
+	def __init__(self):
+		colorama.init()
+	
+	def _log(self, message, style):
+		print(f"{datetime.now()} | {style}{message}{colorama.Style.RESET_ALL}")
 
 	def create(self, item):
-		self.loggers['create'].info(f'-> {item.type}::{item.status}')
+		self._log(f'-> {item.type}::{item.status}', colorama.Fore.GREEN)
 	
 	def update(self, type, id, item):
-		self.loggers['update'].info(f'{type}::?::{id} -> {type}::{item.status}')
+		self._log(f'{type}::?::{id} -> {type}::{item.status}', colorama.Fore.LIGHTBLUE_EX)
 
 	def delete(self, type, id):
-		self.loggers['delete'].info(f'{type}::?::{id} ->')
+		self._log(f'{type}::?::{id} ->', colorama.Fore.RED)
+
+colorama.Fore.
