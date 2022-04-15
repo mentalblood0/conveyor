da1a6a4889ae53f9b28e497262a1d04678517fe4
Нечепоренко Степан <neceporenkostepan@gmail.com>
2022-04-15 14:39:22 +0300

    Creator support for ExceptionLogging effect

--- conveyor/core/Creator.py
+++ conveyor/core/Creator.py
@@ -26,36 +26,41 @@ class Creator(metaclass=ABCMeta):
 	def create(self, *args, **kwargs) -> Item:
 		pass
 
+	def handleException(e: Exception, name: str):
+		pass
+
 	def __call__(self, *args, **kwargs) -> int:
 
 		try:
+
 			item = self.create(*args, **kwargs)
-		except Exception:
-			return 0
 
-		if self.source_type and self.source_status:
+			if self.source_type and self.source_status:
 
-			try:
-				chain_id = self.repository.get(
-					type=self.source_type,
-					where={
-						key: item.metadata[key]
-						for key in self.match_fields
-					},
-					fields=['chain_id']
-				)[0].chain_id
+				try:
+					chain_id = self.repository.get(
+						type=self.source_type,
+						where={
+							key: item.metadata[key]
+							for key in self.match_fields
+						},
+						fields=['chain_id']
+					)[0].chain_id
 
-			except IndexError:
+				except IndexError:
+					chain_id = composeChainId()
+			
+			else:
 				chain_id = composeChainId()
-		
-		else:
-			chain_id = composeChainId()
 
-		return self.repository.create(
-			dataclasses.replace(
-				item,
-				type=self.output_type,
-				status=self.output_status,
-				chain_id=chain_id
+			return self.repository.create(
+				dataclasses.replace(
+					item,
+					type=self.output_type,
+					status=self.output_status,
+					chain_id=chain_id
+				)
 			)
-		)
+		
+		except Exception as e:
+			self.handleException(e, self.__class__.__name__)
