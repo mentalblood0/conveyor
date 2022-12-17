873dff234de242e633815bd67c4eed43fc861ad7
mentalblood <neceporenkostepan@gmail.com>
2022-12-17 14:13:08 +0300

    add benchmarks for different digests methods

---
+++ benchmarks/benchmark_digest.py
@@ -0,0 +1,60 @@
+import random
+import hashlib
+import functools
+from blake3 import blake3
+from sharpener_lite import Benchmark
+
+from benchmarks.common import *
+
+
+
+class blake3_(Benchmark, WithOutputMetrics):
+
+	def prepare(self):
+		self.input_
+
+	@functools.cached_property
+	def input_(self) -> bytes:
+		return ''.join(
+			random.choice('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*(){}[]')
+			for i in range(self.config.kwargs['input_size_kilobytes'] * 1024)
+		).encode()
+	
+	def run(self):
+		return blake3(self.input_).digest()
+
+
+class sha3_512(Benchmark, WithOutputMetrics):
+
+	def prepare(self):
+		self.input_
+
+	@functools.cached_property
+	def input_(self) -> bytes:
+		return ''.join(
+			random.choice('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*(){}[]')
+			for i in range(self.config.kwargs['input_size_kilobytes'] * 1024)
+		).encode()
+	
+	def run(self):
+		return hashlib.sha3_512(
+			self.input_
+		).digest()
+
+
+class sha3_256(Benchmark, WithOutputMetrics):
+
+	def prepare(self):
+		self.input_
+
+	@functools.cached_property
+	def input_(self) -> bytes:
+		return ''.join(
+			random.choice('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*(){}[]')
+			for i in range(self.config.kwargs['input_size_kilobytes'] * 1024)
+		).encode()
+	
+	def run(self):
+		return hashlib.sha3_256(
+			self.input_
+		).digest()
