421b4bfe7109704f1f31ff89efb74ecb030d9f6f
mentalblood <neceporenkostepan@gmail.com>
2022-12-24 10:31:57 +0300

    express `Limit` using `pydantic.PositiveInt`

--- conveyor/core/ItemQuery.py
+++ conveyor/core/ItemQuery.py
@@ -7,14 +7,8 @@ from .ItemMask import ItemMask
 @pydantic.dataclasses.dataclass(frozen=True, kw_only=True)
 class ItemQuery:
 
-	Limit = int
+	Limit = pydantic.PositiveInt
 	Mask = ItemMask
 
 	mask: ItemMask
 	limit: Limit
-
-	@pydantic.validator('limit')
-	def limit_valid(cls, limit: Limit) -> Limit:
-		if limit <= 0:
-			raise ValueError(f"Query result limit must be greater then 0 (received {limit} <= 0)")
-		return limit
