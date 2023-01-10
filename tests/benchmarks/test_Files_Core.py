d14933852fd3d20643b3c143135aa56b38065353
Нечепоренко Степан <neceporenkostepan@gmail.com>
2023-01-10 17:12:30 +0300

    add `test_add_different_big`

--- tests/benchmarks/test_Files_Core.py
+++ tests/benchmarks/test_Files_Core.py
@@ -1,4 +1,5 @@
 import pytest
+import typing
 import pathlib
 
 from tests.common import *
@@ -26,4 +27,22 @@ def big() -> Data:
 @pytest.mark.benchmark(group='files')
 def test_add_same_big(benchmark, files: Files.Core, big: Data):
 	benchmark(files.append, big)
+	files.clear()
+
+
+def append_different(files: Files.Core, data: Data) -> typing.Callable[[], None]:
+
+	current_data = data
+
+	def append():
+		nonlocal current_data
+		files.append(current_data)
+		current_data = Data(value = current_data.value + b' ')
+
+	return append
+
+
+@pytest.mark.benchmark(group='files')
+def test_add_different_big(benchmark, files: Files.Core, big: Data):
+	benchmark(append_different(files, big))
 	files.clear()
