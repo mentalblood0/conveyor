f3e63aafeab427a88f298f4093409b8094f6e5f9
Нечепоренко Степан <neceporenkostepan@gmail.com>
2023-01-13 11:54:40 +0300

    correct indexing ; fixes for consistent working with sqlite and postgresql

--- conveyor/repositories/Rows/Table.py
+++ conveyor/repositories/Rows/Table.py
@@ -31,31 +31,46 @@ base_fields: tuple[
 )
 
 
-@pydantic.validate_arguments
-def Column(name: BaseField | Item.Metadata.Key, value: Item.Metadata.Value):
-	match name:
-		case 'status':
-			return sqlalchemy.Column(name, sqlalchemy.String(63), primary_key=True, nullable=False)
-		case 'digest':
-			return sqlalchemy.Column(name, sqlalchemy.String(127), primary_key=True, nullable=False)
-		case 'chain':
-			return sqlalchemy.Column(name, sqlalchemy.String(127), primary_key=True, nullable=False)
-		case 'created':
-			return sqlalchemy.Column(name, sqlalchemy.DateTime(timezone=False), primary_key=True, nullable=False)
-		case 'reserver':
-			return sqlalchemy.Column(name, sqlalchemy.String(31), primary_key=True, nullable=True)
-		case Item.Metadata.Key():
-			match value:
-				case str():
-					return sqlalchemy.Column(name.value, sqlalchemy.String(255), primary_key=True)
-				case int():
-					return sqlalchemy.Column(name.value, sqlalchemy.Integer(), primary_key=True)
-				case float():
-					return sqlalchemy.Column(name.value, sqlalchemy.Float(), primary_key=True)
-				case datetime.datetime() | type(datetime.datetime()):
-					return sqlalchemy.Column(name.value, sqlalchemy.DateTime(timezone=False), primary_key=True)
-				case None:
-					raise ValueError(f'Can not guess column type corresponding to value with type `{type(value)}`')
+@pydantic.dataclasses.dataclass(frozen=True, kw_only=True)
+class Field:
+
+	name: BaseField | Item.Metadata.Key
+	value: Item.Metadata.Value
+
+	@property
+	def column(self) -> sqlalchemy.Column[typing.Any]:
+		match self.name:
+			case 'status':
+				return         sqlalchemy.Column(self.name,       sqlalchemy.String(127), nullable = False)
+			case 'digest':
+				return         sqlalchemy.Column(self.name,       sqlalchemy.String(127), nullable = False)
+			case 'chain':
+				return         sqlalchemy.Column(self.name,       sqlalchemy.String(127), nullable = False)
+			case 'created':
+				return         sqlalchemy.Column(self.name,       sqlalchemy.DateTime(timezone=False), nullable = False)
+			case 'reserver':
+				return         sqlalchemy.Column(self.name,       sqlalchemy.String(31),  nullable = True)
+			case Item.Metadata.Key():
+				match self.value:
+					case str():
+						return sqlalchemy.Column(self.name.value, sqlalchemy.String(255), nullable = True)
+					case int():
+						return sqlalchemy.Column(self.name.value, sqlalchemy.Integer(),   nullable = True)
+					case float():
+						return sqlalchemy.Column(self.name.value, sqlalchemy.Float(),     nullable = True)
+					case datetime.datetime() | type(datetime.datetime()):
+						return sqlalchemy.Column(self.name.value, sqlalchemy.DateTime(timezone=False), nullable = True)
+					case None:
+						raise ValueError(f'Can not guess column type corresponding to value with type `{type(self.value)}`')
+
+	@pydantic.validate_arguments(config = {'arbitrary_types_allowed': True})
+	def index(self, table: sqlalchemy.Table) -> sqlalchemy.Index:
+		match self.name:
+			case Item.Metadata.Key():
+				return sqlalchemy.Index(f'index__{self.name.value}', self.name.value, _table = table)
+			case _:
+				return sqlalchemy.Index(f'index__{self.name}', self.name, _table = table)
+
 
 
 tables = sqlalchemy.MetaData()
@@ -91,21 +106,35 @@ def Table(
 
 			with connection.begin_nested() as _:
 
-				result = sqlalchemy.Table(
-					name.value,
-					tables,
-					*(
-						Column(name, None)
-						for name in base_fields
-					),
-					*(
-						Column(k, v)
-						for k, v in metadata.value.items()
-					),
-					extend_existing=True
-				)
+				fields = [
+					Field(name = n, value = None)
+					for n in base_fields
+				] + [
+					Field(name = k, value = v)
+					for k, v in metadata.value.items()
+				]
 
 				if name.value not in sqlalchemy.inspect(connection).get_table_names():
-					result.create(bind=connection)
 
-			return result
+					result = sqlalchemy.Table(
+						name.value,
+						sqlalchemy.MetaData(),
+						*(f.column for f in fields)
+					)
+					result.create(bind = connection)
+
+					for f in fields:
+						i = f.index(result)
+						if i not in result.indexes:
+							i.create(bind = connection, checkfirst = True)
+
+					return result
+
+				else:
+
+					return sqlalchemy.Table(
+						name.value,
+						tables,
+						*(f.column for f in fields),
+						extend_existing = True
+					)
