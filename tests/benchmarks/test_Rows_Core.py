be2e4e3a59e8eeb6b1021b91cebb371eed3402d5
mentalblood <neceporenkostepan@gmail.com>
2023-01-14 15:34:18 +0300

    add benchmark for getting row from table

--- tests/benchmarks/test_Rows_Core.py
+++ tests/benchmarks/test_Rows_Core.py
@@ -1,5 +1,3 @@
-import pytest
-
 from tests.common import *
 
 
@@ -7,4 +5,11 @@ from tests.common import *
 @pytest.mark.benchmark(group='rows')
 def test_add_same(benchmark, rows: Rows.Core, row: Rows.Core.Item):
 	benchmark(rows.append, row)
+	rows.clear()
+
+
+@pytest.mark.benchmark(group='rows')
+def test_get_same_one(benchmark, rows: Rows.Core, row: Rows.Core.Item, query_all: Query):
+	rows.append(row)
+	benchmark(lambda: rows.__getitem__(query_all).__iter__().__next__())
 	rows.clear()
