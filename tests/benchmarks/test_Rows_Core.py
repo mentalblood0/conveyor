ac559a55656ed24c0c7382963c34f9a352887c65
mentalblood <neceporenkostepan@gmail.com>
2023-01-14 21:02:17 +0300

    add benchmark for getting next row in query results

--- tests/benchmarks/test_Rows_Core.py
+++ tests/benchmarks/test_Rows_Core.py
@@ -1,3 +1,4 @@
+import dataclasses
 from tests.common import *
 
 
@@ -9,7 +10,32 @@ def test_add_same(benchmark, rows: Rows.Core, row: Rows.Core.Item):
 
 
 @pytest.mark.benchmark(group='rows')
-def test_get_same_one(benchmark, rows: Rows.Core, row: Rows.Core.Item, query_all: Query):
+def test_get_same_first(benchmark, rows: Rows.Core, row: Rows.Core.Item, query_all: Query):
 	rows.append(row)
 	benchmark(lambda: rows.__getitem__(query_all).__iter__().__next__())
+	rows.clear()
+
+
+def get_next(items: typing.Iterator[Rows.Core.Item]) -> typing.Callable[[], Rows.Core.Item]:
+
+	def f():
+		nonlocal items
+		return items.__next__()
+
+	return f
+
+
+@pytest.mark.benchmark(group='rows')
+def test_get_same_next(benchmark, rows: Rows.Core, row: Rows.Core.Item, query_all: Query):
+
+	limit = 1000
+	query_limited = dataclasses.replace(query_all, limit = limit)
+	for _ in range(limit):
+		rows.append(row)
+
+	iterator = rows.__getitem__(query_limited).__iter__()
+	iterator.__next__()
+
+	benchmark.pedantic(get_next(iterator), rounds = limit - 1)
+
 	rows.clear()
