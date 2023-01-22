104151ccffa0da8afd7dc6bc6535a95442751e32
mentalblood <neceporenkostepan@gmail.com>
2023-01-22 14:35:59 +0300

    make `__setitem__` and `__delitem__` always transactional ; minor code style improvements

--- conveyor/core/Repository.py
+++ conveyor/core/Repository.py
@@ -23,9 +23,11 @@ class Repository:
 
 	@pydantic.validator('parts')
 	def parts_valid(cls, parts: Parts) -> Parts:
-		if len(parts) < 1:
-			raise ValueError(f'Repository must have at least 1 part ({len(parts)} provided)')
-		return parts
+		match len(parts):
+			case 0:
+				raise pydantic.ListMinLengthError(limit_value = 1)
+			case _:
+				return parts
 
 	@pydantic.validate_arguments
 	def append(self, item: Item) -> None:
@@ -34,39 +36,38 @@ class Repository:
 
 	@pydantic.validate_arguments
 	def _get(self, query: Query, repositories: typing.Sequence[PartRepository], parts: typing.Iterable[Part] = (Part(),)) -> typing.Iterable[Item]:
-
-		if not len(repositories):
-			for p in parts:
-				yield p.item
-			return
-
-		for p in parts:
-			for item in self._get(
-				query=query,
-				repositories=repositories[1:],
-				parts=repositories[0].get(query, p)
-			):
-				yield item
+		match len(repositories):
+			case 0:
+				for p in parts:
+					yield p.item
+			case _:
+				for p in parts:
+					for item in self._get(
+						query        = query,
+						repositories = repositories[1:],
+						parts        = repositories[0].get(query, p)
+					):
+						yield item
 
 	@pydantic.validate_arguments
 	def __getitem__(self, item_query: Query) -> typing.Iterable[Item]:
 
-		reserver = Item.Reserver(exists=True)
+		reserver = Item.Reserver(exists = True)
 
 		for i in itertools.islice(
 			self._get(
-				query=dataclasses.replace(
+				query = dataclasses.replace(
 					item_query,
-					mask=dataclasses.replace(
+					mask = dataclasses.replace(
 						item_query.mask,
-						reserver=Item.Reserver(exists=False)
+						reserver = Item.Reserver(exists = False)
 					)
 				),
-				repositories=self.parts
+				repositories = self.parts
 			),
 			item_query.limit
 		):
-			i_reserved = dataclasses.replace(i, reserver=reserver)
+			i_reserved = dataclasses.replace(i, reserver = reserver)
 			try:
 				self.__setitem__(i, i_reserved, True)
 			except KeyError:
@@ -75,21 +76,25 @@ class Repository:
 
 	@pydantic.validate_arguments
 	def __setitem__(self, old: Item, new: Item, for_reserve: bool = False) -> None:
+
 		if not for_reserve:
-			new = dataclasses.replace(new, reserver=Item.Reserver(exists=False))
-		for p in reversed(self.parts):
-			try:
-				p[old] = new
-			except NotImplementedError:
-				pass
+			new = dataclasses.replace(new, reserver = Item.Reserver(exists = False))
+
+		with self.transaction() as t:
+			for p in reversed(t.parts):
+				try:
+					p[old] = new
+				except NotImplementedError:
+					pass
 
 	@pydantic.validate_arguments
 	def __delitem__(self, item: Item) -> None:
-		for p in self.parts:
-			try:
-				del p[item]
-			except KeyError:
-				break
+		with self.transaction() as t:
+			for p in t.parts:
+				try:
+					del p[item]
+				except KeyError:
+					break
 
 	@pydantic.validate_arguments
 	@contextlib.contextmanager
@@ -101,24 +106,15 @@ class Repository:
 				with parts_to_include[0].transaction() as t:
 					with self._transaction(parts_to_include[1:]) as _t:
 						yield (t, *_t)
-						# yield (parts_to_include[0].transaction(), *_t)
-						# yield (parts_to_include[0].transaction(), *(parts_to_include[1].transaction(), *_t))
 
-	@pydantic.validate_arguments
 	@contextlib.contextmanager
 	def transaction(self) -> typing.Iterator[typing.Self]:
 		with self._transaction(self.parts) as transaction_parts:
-			yield dataclasses.replace(
-				self,
-				parts = transaction_parts
-			)
+			yield dataclasses.replace(self, parts = transaction_parts)
 
 	@pydantic.validate_arguments
 	def __contains__(self, item: Item) -> bool:
 		return all(item in p for p in self.parts)
 
 	def __len__(self) -> pydantic.NonNegativeInt:
-		return max(
-			len(p)
-			for p in self.parts
-		)
+		return max(len(p) for p in self.parts)
