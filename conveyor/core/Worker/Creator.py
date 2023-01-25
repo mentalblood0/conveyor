bcb90b79f976ef3f834f6395dc67e9fb31883ea6
Нечепоренко Степан <neceporenkostepan@gmail.com>
2023-01-25 12:14:22 +0300

    add `Creator`

---
+++ conveyor/core/Worker/Creator.py
@@ -0,0 +1,23 @@
+import abc
+import typing
+import pydantic
+
+from ..Item import Item
+
+from . import Action
+from .Processor import Processor
+
+
+
+@pydantic.dataclasses.dataclass(frozen=True, kw_only=True)
+class Creator(Processor, metaclass = abc.ABCMeta):
+
+	@abc.abstractmethod
+	def process(self, **kwargs: dict[str, typing.Any]) -> typing.Iterable[Item]:
+		pass
+
+	@pydantic.validate_arguments
+	@typing.final
+	def __call__(self, input: typing.Iterable[Item], **kwargs: dict[str, typing.Any]) -> typing.Iterable[Action.Action]:
+		for o in self.process(**kwargs):
+			yield Action.Append(o)
