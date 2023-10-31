dfdf8e47ee332d326944b39c325e7de3b42df57b
Нечепоренко Степан <neceporenkostepan@gmail.com>
2023-10-31 16:47:17 +0300

    simplicity related refactoring for Transformer

--- conveyor/core/Worker/processors/Transformer.py
+++ conveyor/core/Worker/processors/Transformer.py
@@ -14,25 +14,27 @@ class Transformer(Processor[Item, Action.Action], metaclass=abc.ABCMeta):
     def process(self, input: Item) -> typing.Iterable[Item.Status | Item.Metadata]:
         pass
 
+    def _process(self, i: Item):
+        try:
+            for o in self.process(i):
+                match o:
+                    case Item.Status():
+                        yield Action.Update(old=i, new=dataclasses.replace(i, status=o))
+                    case Item.Metadata():
+                        yield Action.Update(
+                            old=i,
+                            new=dataclasses.replace(i, metadata=i.metadata | o),
+                        )
+        except Exception as e:
+            raise self.error(i, e)
+
+        yield Action.Success(i)
+
     @typing.final
     def __call__(
         self, input: typing.Callable[[], typing.Iterable[Item]], config: typing.Any = {}
     ) -> typing.Iterable[Action.Action]:
         for i in input():
-            try:
-                for o in self.process(i):
-                    match o:
-                        case Item.Status():
-                            yield Action.Update(
-                                old=i, new=dataclasses.replace(i, status=o)
-                            )
-                        case Item.Metadata():
-                            yield Action.Update(
-                                old=i,
-                                new=dataclasses.replace(i, metadata=i.metadata | o),
-                            )
-            except Exception as e:
-                raise self.error(i, e)
-
-            yield Action.Success(i)
+            for r in self._process(i):
+                yield r
             break
