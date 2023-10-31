6a963f423212d27fd91be57616539cd7444c9719
Нечепоренко Степан <neceporenkostepan@gmail.com>
2023-10-31 16:51:42 +0300

    simplicity related refactoring for Synthesizer

--- conveyor/core/Worker/processors/Synthesizer.py
+++ conveyor/core/Worker/processors/Synthesizer.py
@@ -16,6 +16,28 @@ class Synthesizer(Processor[Item, Action.Action], metaclass=abc.ABCMeta):
     ) -> typing.Iterable[Item.Status | Item]:
         pass
 
+    def _actions(self, i: Item, o: Item.Status | Item):
+        match o:
+            case Item.Status():
+                yield Action.Update(old=i, new=dataclasses.replace(i, status=o))
+            case Item():
+                if o.chain != i.chain:
+                    raise ValueError(
+                        f"Output chain ({o.chain}) must be equal "
+                        f"to input chain ({i.chain})"
+                    )
+                yield Action.Append(o)
+
+    def _process(self, items: typing.Iterator[Item], i: Item):
+        try:
+            for o in self.process(i, iter(items)):
+                for a in self._actions(i, o):
+                    yield a
+        except Exception as e:
+            raise self.error(i, e)
+
+        yield Action.Success(i)
+
     @typing.final
     def __call__(
         self, input: typing.Callable[[], typing.Iterable[Item]], config: typing.Any = {}
@@ -23,22 +45,6 @@ class Synthesizer(Processor[Item, Action.Action], metaclass=abc.ABCMeta):
         iterator = iter(input())
 
         for i in iterator:
-            try:
-                for o in self.process(i, iter(iterator)):
-                    match o:
-                        case Item.Status():
-                            yield Action.Update(
-                                old=i, new=dataclasses.replace(i, status=o)
-                            )
-                        case Item():
-                            if o.chain != i.chain:
-                                raise ValueError(
-                                    f"Output chain ({o.chain}) must be equal "
-                                    f"to input chain ({i.chain})"
-                                )
-                            yield Action.Append(o)
-            except Exception as e:
-                raise self.error(i, e)
-
-            yield Action.Success(i)
+            for r in self._process(iterator, i):
+                yield r
             break
