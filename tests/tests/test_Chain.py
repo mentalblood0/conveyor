724be370527dede8364aec8afd11a6be429a39ea
Нечепоренко Степан <neceporenkostepan@gmail.com>
2023-11-03 12:21:24 +0300

    fix coverage for Chain.py

--- tests/tests/test_Chain.py
+++ tests/tests/test_Chain.py
@@ -3,5 +3,10 @@ from conveyor.core import Item
 from ..common import *
 
 
-def test_chain_equal(item: Item):
+def test_chain_equal_self(item: Item):
     assert Item.Chain(ref=item.data) == Item.Chain(ref=item.data)
+
+
+def test_chain_not_equal_not_chain(item: Item):
+    with pytest.raises(TypeError):
+        assert Item.Chain(ref=item.data) == "something"
